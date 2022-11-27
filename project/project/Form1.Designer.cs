3756ee62403fb3d931d90991aa69759bb736f9f9
youstina662000 <67027661+youstina662000@users.noreply.github.com>
2022-11-27 19:16:45 +0200

    Add files via upload

---
+++ project/project/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.singlePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multipictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.slideShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.singlePictureToolStripMenuItem,
+            this.multipictureToolStripMenuItem,
+            this.slideShowToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(146, 92);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // singlePictureToolStripMenuItem
+            // 
+            this.singlePictureToolStripMenuItem.Name = "singlePictureToolStripMenuItem";
+            this.singlePictureToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.singlePictureToolStripMenuItem.Text = "single picture";
+            this.singlePictureToolStripMenuItem.Click += new System.EventHandler(this.singlePictureToolStripMenuItem_Click);
+            // 
+            // multipictureToolStripMenuItem
+            // 
+            this.multipictureToolStripMenuItem.Name = "multipictureToolStripMenuItem";
+            this.multipictureToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.multipictureToolStripMenuItem.Text = "multi_picture";
+            this.multipictureToolStripMenuItem.Click += new System.EventHandler(this.multipictureToolStripMenuItem_Click);
+            // 
+            // slideShowToolStripMenuItem
+            // 
+            this.slideShowToolStripMenuItem.Name = "slideShowToolStripMenuItem";
+            this.slideShowToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.slideShowToolStripMenuItem.Text = "slide show";
+            this.slideShowToolStripMenuItem.Click += new System.EventHandler(this.slideShowToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(991, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(85, 446);
+            this.listBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.statusStrip1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(973, 422);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(976, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(973, 419);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 488);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem singlePictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multipictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem slideShowToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ project/project/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        List<string> SelImg;
+        List<string> AllImg;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            SelImg = new List<string>();
+            AllImg = new List<string>();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+          
+            t.Stop();
+
+        }
+       
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+          
+            
+            
+      }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "image(*.BMP;*.JPG;.GIF;*.PNG;*.TIFF)|*.BMP;*.JPG;.GIF;*.PNG;*.TIFF";
+            op.Multiselect = true;
+            DialogResult o = op.ShowDialog();
+            if (o == DialogResult.OK)
+            {
+                for (int i = 0; i < op.FileNames.Length; i++)
+                {
+                    AllImg.Add(op.FileNames[i]);
+                   
+                    listBox1.Items.Add(Path.GetFileNameWithoutExtension(AllImg[i]));
+                }
+               
+            }
+        }
+
+        private void singlePictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip1.Hide();
+           
+            pictureBox1.Show();
+            t.Stop();
+            SelImg.Clear();
+            listBox1.SelectionMode = SelectionMode.One;
+            SelImg.Add(AllImg[listBox1.SelectedIndex]);
+           
+
+            pictureBox1.ImageLocation = SelImg[0];
+            
+
+        }
+
+         void slideShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Show();
+            
+
+            SelImg.Clear();
+            statusStrip1.Show();
+
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            foreach (int i in listBox1.SelectedIndices)
+            {
+
+                SelImg.Add(AllImg[i]);
+
+            }
+            
+            t.Tick += T_Tick;
+            t.Interval = 1500;
+            t.Enabled = true;
+            t.Start();
+
+        }
+        int c = 0;
+
+          private void T_Tick(object sender, System.EventArgs e)
+        {
+            
+            pictureBox1.ImageLocation = SelImg[c];
+            toolStripStatusLabel1.Text = Path.GetFileNameWithoutExtension(SelImg[c]);
+            c++;
+            c = c % SelImg.Count;
+        }
+
+        private void multipictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip1.Hide();
+            pictureBox1.Hide();
+            panel1.Show();
+            SelImg.Clear();
+            
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+           
+            foreach (int i in listBox1.SelectedIndices)
+            {
+
+                SelImg.Add(AllImg[i]);
+
+            }
+           
+            int x = 0, y = 0;
+            for (int i = 0; i < SelImg.Count; i++)
+            {
+                PictureBox p = new PictureBox()
+                { SizeMode = PictureBoxSizeMode.Zoom };
+                p.Location = new Point(x, y);
+                p.Width = panel1.Width / 5;
+                p.Height = panel1.Height / 5;
+               
+                    p.Image = new Bitmap(SelImg[i]);
+                    panel1.Controls.Add(p);
+
+                    if ((x + p.Width + 5) >= panel1.Width)
+                    {
+                        x = 0;
+                        y += p.Height + 1;
+                    }
+                    else
+                    {
+                        x += p.Width;
+                    }
+
+                }
+                }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
